19e07214aabdf4ee56a23ae0e65cb4c1df4e5682
Max Ehrlich <max.ehr@gmail.com>
2017-09-22 13:18:29 -0400

    Fix patch setup to take into account rc versions and to search for save versions using heuristic

--- setup-patches.py
+++ setup-patches.py
@@ -8,7 +8,7 @@ kern_urls = {
 }
 
 
-def find_tag(major, minor, patch):
+def find_tag(major, minor, patch, rc):
 
     def look_for(ver):
         format_tag = 'v{0}'
@@ -25,19 +25,68 @@ def find_tag(major, minor, patch):
         else:
             print('No matching patch for {0}'.format(format_ver))
 
-    # step 2: try to find a tag for the kernel maj and min version
+    # step 2: try to find a tag for the kernel maj, min, and rc
+    if rc is not None:
+        format_ver = '{0}.{1}-{2}'.format(major, minor, rc)
+        tag_found = look_for(format_ver)
+        if tag_found != '':
+            return tag_found
+        else:
+            print('No matching rc version for {0}'.format(format_ver))
+
+    # step 3: try to find a tag for the kernel maj and min version
     format_ver = '{0}.{1}'.format(major, minor)
     tag_found = look_for(format_ver)
     if tag_found != '':
         return tag_found
     else:
-        print('No matching version for {0}'.format(format_ver))
+        print('No matching minor version for {0}'.format(format_ver))
 
-    # step 3: try to build from the latest tag, if this fails the patches need updating
+    # step 4: try to build from closest tag less than minor version
+    format_ver = '{0}.{1}'.format(major, '*')
+    tag_found = look_for(format_ver)
+    if tag_found != '':
+        tags = tag_found.splitlines()
+        tags.reverse()
+
+        for tag in tags:
+            query_minor = tag.split('.')[1] if '.' in tag else None
+
+            if query_minor is not None:
+                if int(query_minor) <= int(minor):
+                    print('Using tag {0} which is closest lower minor version'.format(tag))
+                    return tag
+                else:
+                    print('Tag {0} is too new'.format(tag))
+
+    print('No closer tag in the {0} series'.format(major))
+
+    # step 5: try to build from closest tag less than major version
+    format_ver = '*'
+    tag_found = look_for(format_ver)
+    if tag_found != '':
+        tags = tag_found.splitlines()
+        tags.reverse()
+
+        for tag in tags:
+            query_major = tag.split('.')[0] if '.' in tag else tag
+            query_major = query_major[1:]
+
+            if int(query_major) <= int(major):
+                print('Using tag {0} which is closest lower major version'.format(tag))
+                return tag
+            else:
+                print('Tag {0} is too new'.format(tag))
+
+    print('No closer tag for major version {0}'.format(major))
+
+    # step 6: try to build from the latest tag, if this fails the patches need updating
     rec_tag = run(['git', 'describe', '--abbrev=0'], stdout=PIPE).stdout.decode('utf-8').strip()
     print('Using latest tag on branch: {0}'.format(rec_tag))
     return rec_tag
 
+
+dryrun = False
 kern_version = None
 kern_type = None
 if len(sys.argv) < 2:
@@ -47,11 +96,18 @@ else:
     print('Parsing build command: {0}'.format(sys.argv[1]))
     kern_version, kern_type = map(str.strip, sys.argv[1].split(':'))  # '4.10.1', stable
 
+    if len(sys.argv) > 2:
+        if sys.argv[2] == '--dryrun':
+            dryrun = True
+            print('DRYRUN')
+
 if kern_type is None:
     kern_type = 'mainline'
 
 print('Building kernel type: {}'.format(kern_type))
-check_call(['git', 'clone', kern_urls[kern_type], 'linux'])
+
+if not dryrun:
+    check_call(['git', 'clone', kern_urls[kern_type], 'linux'])
 
 if kern_version is None:
     os.chdir('linux')
@@ -64,25 +120,39 @@ kern_vl = kern_version.split('.')  # 4, 10, 1
 kern_maj = kern_vl[0]
 kern_min = kern_vl[1]
 kern_patch = kern_vl[2] if len(kern_vl) > 2 else None
-print('Major: {0}, Minor: {1}, Patch: {2}'.format(kern_maj, kern_min, kern_patch))
+
+if '-' in kern_min:
+    kern_min, kern_rc = kern_min.split('-')
+else:
+    kern_rc = None
+
+print('Major: {0}, Minor: {1}, Patch: {2}, RC: {3}'.format(kern_maj, kern_min, kern_patch, kern_rc))
 
 if kern_patch is not None:
     kern_tag = 'v{0}.{1}.{2}'.format(kern_maj, kern_min, kern_patch)
+elif kern_rc is not None:
+    kern_tag = 'v{0}.{1}-{2}'.format(kern_maj, kern_min, kern_rc)
 else:
     kern_tag = 'v{0}.{1}'.format(kern_maj, kern_min)
 
 # Checkout kernel
-os.chdir('linux')
-check_call(['git', 'checkout', kern_tag])
+if not dryrun:
+    os.chdir('linux')
+
+if not dryrun:
+    check_call(['git', 'checkout', kern_tag])
 
 # Checkout patch
-os.chdir('..')
-acso_tag = find_tag(kern_maj, kern_min, kern_patch)
+if not dryrun:
+    os.chdir('..')
+
+acso_tag = find_tag(kern_maj, kern_min, kern_patch, kern_rc)
 check_call(['git', 'checkout', acso_tag])
 
 # Apply patch
-os.chdir('linux')
-check_call(['git', 'apply', '../acso.patch'])
+if not dryrun:
+    os.chdir('linux')
+    check_call(['git', 'apply', '../acso.patch'])
 
 print('ready to build')
 
