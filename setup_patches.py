400d7f1c2870669116587b5897db142c155911c6
Max Ehrlich <max.ehr@gmail.com>
2017-10-22 16:27:34 -0400

    Refactor setup_patches utility based on database method

--- setup_patches.py
+++ setup_patches.py
@@ -1,129 +1,31 @@
-import sys
 from subprocess import check_call
 import os
-import shutil
-import kern_util
+import kernel
 import argparse
-import glob
-
-
-def select_workspace(major, minor, patch, rc, **kwargs):
-    def format_tag(kspec_partial):
-        ks = kern_util.format_kernel(kspec_partial)
-        return '{}*'.format(ks)
-
-    def look_for(ver):
-        return [d for d in glob.glob(ver) if os.path.isdir(d)]
-
-    def match_exact(major=None, minor=None, patch=None, rc=None):
-        kspec = kern_util.kernel_spec(major, minor, patch, rc)
-        target_tag = format_tag(kspec)
-        tag_found = look_for(target_tag)
-
-        if len(tag_found) == 1:
-            return True, tag_found[0], target_tag
-        else:
-            return False, None, target_tag
-
-    def match_leq(major=None, minor=None):
-        kspec = kern_util.kernel_spec(major=major)
-        target_ws = format_tag(kspec)
-        workspaces = look_for(target_ws)
-
-        if len(workspaces) > 0:
-            workspaces.sort()
-            workspaces.reverse()
-
-            for ws in workspaces:
-                if major is not None:
-                    target = minor
-                    query = ws.split('.')[1] if '.' in ws else None
-                else:
-                    target = major
-                    query = ws.split('.')[0] if '.' in ws else ws
-                    query = query[1:]
-
-                if query is not None:
-                    if int(query) <= int(target):
-                        return True, ws, target_ws
-
-        return False, None, target_ws
-
-    # step 1: try to find a workspace for the specific kernel and patch
-    if patch is not None:
-        found, workspace, target = match_exact(major=major, minor=minor, patch=patch)
-        if found:
-            print('Found exact match for {0}'.format(target))
-            return workspace
-        else:
-            print('No matching patch for {0}'.format(target))
-
-    # step 2: try to find a workspace for the kernel maj, min, and rc
-    if rc is not None:
-        found, workspace, target = match_exact(major=major, minor=minor, rc=rc)
-        if found:
-            print('Found exact match {0} for {1}'.format(workspace, target))
-            return workspace
-        else:
-            print('No matching rc for {0}'.format(target))
-
-    # step 3: try to find a workspace for the kernel maj and min version
-    found, workspace, target = match_exact(major=major, minor=minor)
-    if found:
-        print('Found exact match {0} for {1}'.format(workspace, target))
-        return workspace
-    else:
-        print('No matching minor version for {0}'.format(target))
-
-    # step 4: try to build from closest workspace less than minor version
-    found, workspace, target = match_leq(major=major, minor=minor)
-    if found:
-        print('Using {0} as closest minor version for {1}'.format(workspace, target))
-        return workspace
-    else:
-        print('No matching minor version for {0}'.format(target))
-
-    # step 5: try to build from closest workspace less than major version
-    found, workspace, target = match_leq(major=major)
-    if found:
-        print('Using {0} as closest major version for {1}'.format(workspace, target))
-        return workspace
-    else:
-        print('No lower versions for {0}'.format(target))
-
-    # step 6: try to build from the latest workspace, if this fails the patches need updating
-    dirs = [d for d in os.listdir('.') if os.path.isdir(d) and not d.startswith('.')]
-    dirs.sort()
-    dirs.reverse()
-
-    workspace = dirs[0]
-
-    print('Using latest workspace: {0}'.format(workspace))
-    return workspace
 
 
 def main(args):
     print('Arguments: {}'.format(args))
     print('Parsing build command: {0}'.format(args.kernel_string))
 
-    kspec = kern_util.parse_kernel(args.kernel_string)
+    kernel_version, kernel_type = kernel.KernelVersion.parse(args.kernel_string)
 
     if args.dryrun:
         print('DRYRUN')
 
-    print('Setting up for kernel: {}'.format(kspec))
+    print('Setting up for kernel: {}, {}'.format(kernel_version, kernel_type))
 
-    acso_workspace = select_workspace(**kspec)
+    acso_workspace = kernel.workspace_for(kernel_version)
+    print('Selected workspace {}'.format(acso_workspace.path))
 
     if not args.dryrun:
-        os.chdir(acso_workspace)
-        kern_util.download_kernel_source(kspec)
+        os.chdir(acso_workspace.path)
+        kernel.download_kernel_source(kernel_type)
         os.chdir('linux')
 
-    kern_tag = 'v{}'.format(kern_util.format_kernel(kspec))
+    kern_tag = 'v{}'.format(kernel_version)
     print('Checking out kernel tag {}'.format(kern_tag))
 
-    # Apply patchs
     if not args.dryrun:
         check_call(['git', 'checkout', kern_tag])
 
@@ -139,4 +41,4 @@ if __name__ == '__main__':
     parser.add_argument('--dryrun', action='store_true', help='Only simulate the patch selection process')
     parser.add_argument('kernel_string', help='String description, <kernel_number>:[mainline|stable]')
     args = parser.parse_args()
-    main(args);
+    main(args)
