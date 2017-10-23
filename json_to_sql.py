826d32aacd7fc870a46a5aeb68fedd155290d7fc
Max Ehrlich <max.ehr@gmail.com>
2017-10-23 13:37:48 -0400

    Ingest old json into sql database and generate new json from it

---
+++ json_to_sql.py
@@ -0,0 +1,19 @@
+import kernel
+import json
+
+with open('kernel.json') as f:
+    data = json.load(f)
+
+for s in data['series']:
+    number, _ = kernel.KernelVersion.parse(s['series_number'])
+    number.save()
+
+    ks = kernel.KernelSeries.from_version(number)
+    ks.save()
+
+    for k in s['kernels']:
+        kn, _ = kernel.KernelVersion.parse(k['kernel_number'])
+        kn.save()
+
+        b = kernel.BuiltKernel(version=kn, type=k['kernel_type'], build_job_id=k['build_job_id'], series=ks)
+        b.save()
