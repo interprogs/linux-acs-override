7652e74ef1d388399878c8ff763630a585ad6481
Max Ehrlich <max.ehr@gmail.com>
2017-10-24 10:49:25 -0400

    Split build logging and json generation into seperate tasks

---
+++ scripts/log_build.py
@@ -0,0 +1,30 @@
+import argparse
+import kernel
+
+
+def main(args):
+    print('Storing successful build of {} for job {}'.format(args.kernel_string, args.job_id))
+
+    kernel_version, kernel_type = kernel.KernelVersion.parse(args.kernel_string)
+    kernel_series = kernel.KernelSeries.from_version(kernel_version)
+
+    kernel_version.save()
+    kernel_series.save()
+
+    built_kernel = kernel.BuiltKernel(version=kernel_version,
+                                      type=kernel_type,
+                                      build_job_id=args.job_id,
+                                      series=kernel_series)
+
+    try:
+        built_kernel.save()
+    except Exception as e:
+        print('WARNING: {}'.format(e))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Log a successful build to the database')
+    parser.add_argument('kernel_string', help='String description, <kernel_number>:[mainline|stable]')
+    parser.add_argument('job_id', help='ID of CI job that built the kernel')
+    args = parser.parse_args()
+    main(args)
--- scripts/sql_to_json.py
+++ scripts/sql_to_json.py
@@ -1,35 +1,17 @@
-import argparse
 import json
-
 import kernel
+import argparse
 
 
 def main(args):
-    kernel_version, kernel_type = kernel.KernelVersion.parse(args.kernel_string)
-    kernel_series = kernel.KernelSeries.from_version(kernel_version)
-
-    kernel_version.save()
-    kernel_series.save()
-
-    built_kernel = kernel.BuiltKernel(version=kernel_version,
-                                      type=kernel_type,
-                                      build_job_id=args.job_id,
-                                      series=kernel_series)
-
-    try:
-        built_kernel.save()
-    except Exception as e:
-        print('WARNING: {}'.format(e))
-
+    print('Dumping built kernel data to {}'.format(args.out))
     all_kernels = kernel.built_kernels_dict()
-
-    with open('kernel.json', 'w') as f:
+    with open(args.out, 'w') as f:
         json.dump(all_kernels, f, indent=4)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Build "JSON API cache" for ACSO kernel site')
-    parser.add_argument('kernel_string', help='String description, <kernel_number>:[mainline|stable]')
-    parser.add_argument('job_id', help='ID of CI job that built the kernel')
+    parser = argparse.ArgumentParser(description='Dump contents of built kernel tables to JSON')
+    parser.add_argument('out', help='Path to output JSON file')
     args = parser.parse_args()
     main(args)
